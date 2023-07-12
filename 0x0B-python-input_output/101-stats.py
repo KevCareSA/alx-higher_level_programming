572871aa76445b0bbe7600ff35386fde0748005e
KevCareSA <kev.mkbn2@gmail.com>
2023-07-13 01:00:35 +0200

    python-input_output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-This module is a script that reads from stdin and computes metrics
+"""Reads from standard input and computes metrics.
 """
 
 
-def print_stats(size, codes):
-    """show metrics.
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
     """
     print("File size: {}".format(size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
-
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
     import sys
 
     size = 0
-    codes = {}
+    status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
         for line in sys.stdin:
             if count == 10:
-                print_stats(size, codes)
+                print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
@@ -37,15 +35,15 @@ if __name__ == "__main__":
 
             try:
                 if line[-2] in valid_codes:
-                    if codes.get(line[-2], -1) == -1:
-                        codes[line[-2]] = 1
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
                     else:
-                        codes[line[-2]] += 1
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
-        print_stats(size, codes)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        print_stats(size, codes)
+        print_stats(size, status_codes)
         raise
