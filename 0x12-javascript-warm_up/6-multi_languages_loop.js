9bbc86f188c8284bafb8d31f1b25652413056ed1
KevCareSA <kev.mkbn2@gmail.com>
2023-09-11 21:27:05 +0200

    try again

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// type something
+// a script that prints 3 lines
 const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const line of arr) {
   console.log(line);
